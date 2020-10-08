0c953bdd52bd8f72901cf0adfb4bff76cf1aadae
Arpit <arpit@interfacem.com>
2020-10-08 09:44:30 +0530

    - Initial setup and openweathermap API key added

---
+++ vue-weather-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>Hello Arpit!</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return{
+      api_key: '70b02826a032ea58ec1ecbc29fce2c97'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
