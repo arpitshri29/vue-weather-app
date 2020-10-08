7154938f297a55f2f4d2efaa6c6f1b5c4ae73cc2
Arpit <arpit@interfacem.com>
2020-10-08 10:24:15 +0530

    - Template created and styled

--- vue-weather-app/src/App.vue
+++ vue-weather-app/src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>Hello Arpit!</div>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Enter a city name.."/>
+      </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Indore, MP</div>
+          <div class="date">Thursday, 8 October 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">29°c</div>
+          <div class="weather">Warm</div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
@@ -17,12 +33,90 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body{
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  #app{
+    background-image: url("./assets/Cold.jpg");
+    background-size: cover;
+    background-position: bottom;
+    transition: 0.4s;
+  }
+
+  main{
+    min-height: 100vh;
+    padding: 25px;
+    background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.5));
+  }
+
+  .search-box{
+    width: 100%;
+    margin-bottom: 30px;
+  }
+
+  .search-box .search-bar{
+    display: block;
+    width: 100%;
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+    appearance: none;
+    border: none;
+    outline: none;
+    background-color: rgba(255, 255, 255, .5);
+    border-radius: 0 16px 0 16px;
+    transition: 0.4s;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
+  }
+
+  .search-box .search-bar:focus{
+    background-color: rgba(255, 255, 255, .8);
+    box-shadow: 0 0 16px rgba(0, 0, 0, 0.2);
+  }
+
+  .location-box .location{
+    color: #ffffff;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px rgba(0, 0, 0, 0.2);
+  }
+
+  .location-box .date{
+    color: #ffffff;
+    font-size: 20px;
+    font-weight: 300;
+    text-align: center;
+  }
+
+  .weather-box{
+    text-align: center;
+  }
+
+  .weather-box .temp{
+    display: inline-block;
+    padding: 10px 25px;
+    color: #ffffff;
+    font-size: 100px;
+    font-weight: 900;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, .2);
+    border-radius: 16px;
+    margin: 30px 0;
+    box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  }
+
+  .weather-box .weather{
+    color: #ffffff;
+    font-size: 48px;
+    font-weight: 700;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  }
 </style>
