d27e1d1e3939892112b06eca8bf6f03073036556
Arpit <arpit@interfacem.com>
2020-10-08 18:07:00 +0530

    - Added date - bg image change based on the temperature - Styling

--- vue-weather-app/src/App.vue
+++ vue-weather-app/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app">
+  <div id="app"
+       v-bind:class="typeof weather.main != 'undefined' && weather.main.temp > 24 ? 'hot' : ''"
+  >
     <main>
       <div class="search-box">
         <input
@@ -13,11 +15,11 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
-          <div class="date">Thursday, 8 October 2020</div>
+          <div class="date">{{dateDisplay()}}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">29°c</div>
-          <div class="weather">Warm</div>
+          <div class="temp">{{Math.round(weather.main.temp)}}°c</div>
+          <div class="weather">{{weather.weather[0].main}}</div>
         </div>
       </div>
     </main>
@@ -42,7 +44,7 @@ export default {
   },
   methods:{
       fetchWeather(e){
-          if (e.key == 'Enter'){
+          if (e.key === 'Enter'){
               fetch(`${this.url_base}/weather?q=${this.query}&units=metric&appid=${this.api_key}`)
                   .then(res => res.json())
                   .then(this.setResults)
@@ -52,6 +54,19 @@ export default {
       setResults (results){
           this.weather = results;
           console.log('weather results', results)
+      },
+      dateDisplay(){
+          let d = new Date();
+          let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+          let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+              'August', 'September', 'October', 'November', 'December'];
+
+          let day = days[d.getDay()];
+          let month = months[d.getMonth()];
+          let date = d.getDate();
+          let year = d.getFullYear();
+
+          return `${day} ${date} ${month} ${year}`
       }
   }
 }
@@ -73,6 +88,10 @@ export default {
     background-size: cover;
     background-position: bottom;
     transition: 0.4s;
+  }
+
+  #app.hot{
+    background-image: url("./assets/Hot.jpg");
   }
 
   main{
